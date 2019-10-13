8a46f66cc4ab98aec5845c91b8582bb123939161
Wagner Reis <wagnerreis1997@gmail.com>
2019-10-13 12:30:33 -0300

    Introduzindo a firebase ao projeto

---
+++ src/firebase.js
@@ -0,0 +1,42 @@
+import app from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBJe5U7qDiLa06LqKCf0cpAvE9rtzQ25ug",
+    authDomain: "projetoblog-2493b.firebaseapp.com",
+    databaseURL: "https://projetoblog-2493b.firebaseio.com",
+    projectId: "projetoblog-2493b",
+    storageBucket: "projetoblog-2493b.appspot.com",
+    messagingSenderId: "171946474426",
+    appId: "1:171946474426:web:f639055156ba7373063b83",
+    measurementId: "G-W8HGNFK46P"
+  };
+
+class Firebase{
+    constructor(){
+        app.initializeApp(firebaseConfig);
+    }
+
+    login(email, password){
+        return app.auth().signInWithEmailAndPassword(email, password)
+    }
+
+    async register(nome, email, password){
+        await app.auth().createUserWithEmailAndPassword(email, password)
+        
+        const uid = app.auth().currentUser.uid;
+
+        return app.database().ref('usuarios').child(uid).set({
+            nome: nome
+        })
+    }
+
+    isInitialized(){
+        return new Promise(resolve => {
+            app.auth().onAuthStateChanged(resolve);
+        })
+    }
+}
+
+export default new Firebase();
