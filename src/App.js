6ca5ae8267364a06e5e6403ee7e86a643a15fc76
Wagner Reis <wagnerreis1997@gmail.com>
2019-10-14 18:39:45 -0300

    Contruindo sistema de login

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import firebase from './firebase';
 
 import Home from './components/Home';
 import Header from './components/Header';
+import Login from './components/Login';
 import './global.css';
 
 class App extends Component{
@@ -25,6 +26,7 @@ class App extends Component{
         <Header/>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
         </Switch>
       </BrowserRouter>
     ) : (
