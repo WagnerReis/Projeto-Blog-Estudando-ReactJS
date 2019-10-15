3c3d70013fee9143848ed5c23a649bc12d365383
Wagner Reis <wagnerreis1997@gmail.com>
2019-10-15 19:39:28 -0300

    Upload imagem do post

--- src/components/New/index.js
+++ src/components/New/index.js
@@ -9,12 +9,15 @@ class New extends Component {
         super(props);
         this.state = {
             titulo: '',
-            imagem: '',
+            imagem: null,
+            url: '',
             descricao: '',
             alert: ''
         };
 
         this.cadastrar = this.cadastrar.bind(this);
+        this.handleFile = this.handleFile.bind(this);
+        this.handleUpload = this.handleUpload.bind(this);
     }
 
     componentDidMount() {
@@ -27,21 +30,58 @@ class New extends Component {
     cadastrar = async (e) => {
         e.preventDefault();
 
-        if(this.state.titulo !== '' && this.state.imagem !== '' && this.state.descricao !== ''){
-              let posts = firebase.app.ref('posts');
-              let chave = posts.push().key; 
-              await posts.child(chave).set({
-                  titulo: this.state.titulo,
-                  image: this.state.imagem,
-                  descricao : this.state.descricao,
-                  autor: localStorage.nome
-              });
+        if (this.state.titulo !== '' && this.state.imagem !== '' && this.state.descricao !== '') {
+            let posts = firebase.app.ref('posts');
+            let chave = posts.push().key;
+            await posts.child(chave).set({
+                titulo: this.state.titulo,
+                image: this.state.imagem,
+                descricao: this.state.descricao,
+                autor: localStorage.nome
+            });
 
-              this.props.history.push('/dashboard');
-        }else{
-            this.setState({alert: 'Preencha todos os campos!'})
+            this.props.history.push('/dashboard');
+        } else {
+            this.setState({ alert: 'Preencha todos os campos!' })
         }
 
+    }
+
+    handleFile = async (e) => {
+
+        if(e.target.files[0]){
+          
+          const image = e.target.files[0];
+    
+          if(image.type === 'image/png' || image.type === 'image/jpeg'){
+            await this.setState({imagem: image});
+            await this.handleUpload();
+          }else{
+            alert('Envie uma imagem do tipo PNG ou JPG');
+            this.setState({imagem: null});
+            return null;
+            }
+        }
+    }
+    handleUpload = async () => {
+        const { imagem } = this.state;
+        const currentUid = firebase.getCurrentUid();
+    
+        const uploadTaks = firebase.storage
+        .ref(`images/${currentUid}/${imagem.name}`)
+        .put(imagem);
+
+        /*await uploadTaks.on('state_changed',
+        (snapshot) => {
+            //progress
+        }),
+        (error)=>{
+            //error
+            console.log('Error imagem: ' + error);
+        },
+        ()=>{
+            //sucesso!
+        }*/
     }
 
     render() {
@@ -51,14 +91,15 @@ class New extends Component {
                     <Link to="/dashboard">Voltar</Link>
                 </header>
                 <form onSubmit={this.cadastrar} id="new-post">
+
                     <span>{this.state.alert}</span>
+
+                    <input type="file"
+                        onChange={this.handleFile} /><br />
+
                     <label>Titulo: </label><br />
                     <input type="text" placeholder="Nome do post" value={this.state.titulo} autoFocus
                         onChange={(e) => this.setState({ titulo: e.target.value })} /><br />
-
-                    <label>Url da imagem: </label><br />
-                    <input type="text" placeholder="Url da capa" value={this.state.imagem}
-                        onChange={(e) => this.setState({ imagem: e.target.value })} /><br />
 
                     <label>Descrição: </label><br />
                     <textarea type="text" placeholder="Alguma descrição" value={this.state.descricao} autoFocus
