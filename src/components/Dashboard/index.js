9e9ecf7a8f4e7418e20913a9a63904c2aedb7da9
Wagner Reis <wagnerreis1997@gmail.com>
2019-10-15 15:48:30 -0300

    Criando Painel Administrativo

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,11 +1,48 @@
 import React, {Component} from 'react';
+import {Link, withRouter} from 'react-router-dom';
+import firebase from '../../firebase';
 
-export default class DashBoard extends Component{
-    render(){
-        return(
-            <div>
-                <h1>Tela Painel</h1>
-            </div>
-        );
+class Dashboard extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: localStorage.nome
+    };
+
+    this.logout = this.logout.bind(this);
+  }
+
+  async componentDidMount(){
+    if(!firebase.getCurrent()){
+      this.props.history.replace('/login');
+      return null;
     }
+
+    firebase.getUserName((info)=>{
+      localStorage.nome = info.val().nome;
+      this.setState({nome: localStorage.nome });
+    })
+
+
+  }
+
+  logout(){
+
+  }
+
+  render(){
+    return(
+      <div id="dashboard">
+        <div className="user-info">
+          <h1>Olá {this.state.nome}</h1>
+          <Link to="/dashboard/new">Novo Post</Link>
+        </div>
+        <p>Logado com: {firebase.getCurrent()}</p>
+        <button onClick={()=> this.logout()}>Deslogar</button>
+      </div>
+    );
+  }
 }
+
+export default withRouter(Dashboard);
