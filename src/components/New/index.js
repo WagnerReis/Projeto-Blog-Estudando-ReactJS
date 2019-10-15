a04039b6e31a3a58acba9728cafe2e831222b374
Wagner Reis <wagnerreis1997@gmail.com>
2019-10-15 18:04:17 -0300

    Criando pagina Novo post pt2

--- src/components/New/index.js
+++ src/components/New/index.js
@@ -1,44 +1,70 @@
-import React, {Component} from 'react';
-import  {Link, withRouter} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import firebase from '../../firebase';
 import './new.css';
 
-class New extends Component{
+class New extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             titulo: '',
             imagem: '',
-            descricao: ''
+            descricao: '',
+            alert: ''
         };
 
         this.cadastrar = this.cadastrar.bind(this);
     }
 
-    cadastrar(){
+    componentDidMount() {
+        if (!firebase.getCurrent()) {
+            this.props.history.replace('/login');
+            return null;
+        }
+    }
+
+    cadastrar = async (e) => {
+        e.preventDefault();
+
+        if(this.state.titulo !== '' && this.state.imagem !== '' && this.state.descricao !== ''){
+              let posts = firebase.app.ref('posts');
+              let chave = posts.push().key; 
+              await posts.child(chave).set({
+                  titulo: this.state.titulo,
+                  image: this.state.imagem,
+                  descricao : this.state.descricao,
+                  autor: localStorage.nome
+              });
+
+              this.props.history.push('/dashboard');
+        }else{
+            this.setState({alert: 'Preencha todos os campos!'})
+        }
 
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <header id="new">
                     <Link to="/dashboard">Voltar</Link>
                 </header>
                 <form onSubmit={this.cadastrar} id="new-post">
-                    <label>Titulo: </label><br/>
+                    <span>{this.state.alert}</span>
+                    <label>Titulo: </label><br />
                     <input type="text" placeholder="Nome do post" value={this.state.titulo} autoFocus
-                     onChange={(e) => this.setState({titulo: e.target.value})}/><br/>
+                        onChange={(e) => this.setState({ titulo: e.target.value })} /><br />
 
-                    <label>Url da imagem: </label><br/>
-                    <input type="text" placeholder="Url da capa" value={this.state.imagem} 
-                     onChange={(e) => this.setState({imagem: e.target.value})}/><br/>
+                    <label>Url da imagem: </label><br />
+                    <input type="text" placeholder="Url da capa" value={this.state.imagem}
+                        onChange={(e) => this.setState({ imagem: e.target.value })} /><br />
 
-                    <label>Descrição: </label><br/>
+                    <label>Descrição: </label><br />
                     <textarea type="text" placeholder="Alguma descrição" value={this.state.descricao} autoFocus
-                     onChange={(e) => this.setState({descricao: e.target.value})}/><br/>
+                        onChange={(e) => this.setState({ descricao: e.target.value })} /><br />
 
-                     <button type="submit">Cadastrar</button>
+                    <button type="submit">Cadastrar</button>
                 </form>
             </div>
         );
